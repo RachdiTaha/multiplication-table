86ff0ff5b677ece605d72e0af2a7e217fcb11c74
RachdiTaha <rachditaha@gmail.com>
2022-04-05 02:21:51 +0000

    second commit

--- script.js
+++ script.js
@@ -6,5 +6,20 @@ function createTable() {
 
   if (inputValue <= 0 || inputValue > 1000) {
     alert("veuillez saisir un nombre entre 1 et 1000");
+  } else {
+    tableau.innerText = " ";
+    tableau.style.display = "block";
+    for (i = 0; i <= 10; i++) {
+      let resultat = `${inputValue} * ${i} = ${inputValue * i} <hr>`;
+      tableau.innerHTML += resultat;
+    }
+    text.value = " ";
+    reset.style.display = "block";
   }
+  const resetTableau = () => {
+    let tableau = document.getElementById("tableau");
+    let reset = document.getElementById("reset");
+    tableau.style.display = "none";
+    reset.style.display = "none";
+  };
 }
